3cd4ca99fe7ebb7e211760b069c2cd0756594c34
Simon Stastny <stastny.simon@gmail.com>
2015-04-28 22:38:46 +0200

    adding subcategories safely

--- src/main/java/org/ubicollab/ubibazaar/core/Category.java
+++ src/main/java/org/ubicollab/ubibazaar/core/Category.java
@@ -8,6 +8,7 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import com.fasterxml.jackson.annotation.JsonAutoDetect;
+import com.google.common.collect.Sets;
 
 @Data
 @Builder
@@ -15,13 +16,25 @@ import com.fasterxml.jackson.annotation.JsonAutoDetect;
 @AllArgsConstructor
 @JsonAutoDetect
 public class Category {
-  
+
   String id;
-  
+
   String name;
-  
+
   Category superCategory;
-  
+
   Set<Category> subCategories;
+
+  public void addSubCategory(Category category) {
+    if (subCategories == null) {
+      synchronized (this) {
+        if (subCategories == null) {
+          subCategories = Sets.newHashSet();
+        }
+      }
+    }
+
+    subCategories.add(category);
+  }
 
 }
